a23e6da32ea80449b76cc98573b481a9434f8ed3
Milla <milla.kortelainen@helsinki.fi>
2019-11-04 10:19:35 +0200

    Viimeinen tehtävä

---
+++ NhlStatistics1/src/test/java/ohtuesimerkki/StatisticsTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StatisticsTest {
+
+    Reader readerStub = new Reader() {
+
+        public List<Player> getPlayers() {
+            ArrayList<Player> players = new ArrayList<>();
+
+            players.add(new Player("Semenko", "EDM", 4, 12));
+            players.add(new Player("Lemieux", "PIT", 45, 54));
+            players.add(new Player("Kurri", "EDM", 37, 53));
+            players.add(new Player("Yzerman", "DET", 42, 56));
+            players.add(new Player("Gretzky", "EDM", 35, 89));
+
+            return players;
+        }
+    };
+
+    Statistics stats;
+
+    @Before
+    public void setUp() {
+        // luodaan Statistics-olio joka käyttää "stubia"
+        stats = new Statistics(readerStub);
+    }
+
+    @Test
+    public void teamExists() {
+        List teams = stats.team("EDM");
+        assertEquals(3, teams.size());
+
+    }
+
+    @Test
+    public void toptier() {
+        List teams = stats.topScorers(1);
+        assertEquals(1, teams.size());
+    }
+
+    @Test
+    public void searching() {
+        Player player = stats.search("Kurri");
+        assertEquals("Kurri", player.getName());
+    }
+
+    @Test
+    public void notFound() {
+        Player player = stats.search("Heikki");
+        assertNull(player);
+
+    }
+}
