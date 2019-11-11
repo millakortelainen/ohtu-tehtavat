5295239e89c6a80f4076802b00489e7585d4defd
Milla <milla.kortelainen@helsinki.fi>
2019-11-11 21:13:31 +0200

    spring on kivaa

---
+++ Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,56 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Kauppa implements KauppaInterface {
+
+    private VarastoInterface varasto;
+    private PankkiInterface pankki;
+    private Ostoskori ostoskori;
+    private ViitegeneraattoriInterface viitegeneraattori;
+    private String kaupanTili;
+
+//    public Kauppa() {
+//        varasto = Varasto.getInstance();
+//        pankki = Pankki.getInstance();
+//        viitegeneraattori = Viitegeneraattori.getInstance();
+//        kaupanTili = "33333-44455";
+//    }
+    @Autowired
+    public Kauppa(VarastoInterface varasto, PankkiInterface pankki, ViitegeneraattoriInterface viitegeneraattori) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
+    }
+
+    @Override
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    @Override
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id);
+        varasto.palautaVarastoon(t);
+    }
+
+    @Override
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id) > 0) {
+            Tuote t = varasto.haeTuote(id);
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    @Override
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
